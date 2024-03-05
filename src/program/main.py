71794e06901ab0b8dc176141c7895dda0bb19793
finn-nankivell2 <N00221795@iadt.ie>
2024-03-05 12:27:33 +0000

    Card snapping

--- src/program/main.py
+++ src/program/main.py
@@ -3,6 +3,7 @@ import pygame
 import pygame.locals
 from pygame import Surface, Vector2, Rect, FRect, Color
 import os
+
 pygame.init()
 
 import sys
@@ -25,20 +26,10 @@ from gameutil import *
 from dataclasses import dataclass
 
 
-
 class DebugRect():
+
 	def update_draw(self):
 		pygame.draw.rect(game.windowsystem.screen, Color("#ff00ff"), self.rect)
-
-
-
-class Hand(Sprite):
-	LAYER = "FOREGROUND"
-
-	def __init__(self, rect):
-		self.rect = rect
-		self.currently_dragged = None
-
 
 
 class Card(Sprite):
@@ -52,11 +43,14 @@ class Card(Sprite):
 		self.mouse_offset = Vector2(0, 0)
 		self.held_frames = 0
 
+	def mouse_over_me(self):
+		return self.rect.collidepoint(game.input.mouse_pos())
+
 	def update_move(self):
 		hand = game.sprites.HAND
 
 		if game.input.mouse_pressed(0) and hand.currently_dragged is None:
-			if self.rect.collidepoint(game.input.mouse_pos()) and not self.dragged:
+			if self.mouse_over_me() and not self.dragged:
 				self.dragged = True
 				self.hand_locked = False
 				self.mouse_offset = game.input.mouse_pos() - self.rect.topleft
@@ -75,15 +69,16 @@ class Card(Sprite):
 			self.rect.topleft = game.input.mouse_pos() - self.mouse_offset
 			self.held_frames += 1
 
-
 		if self.hand_locked:
-			travel = (hand.rect.bottom - self.rect.bottom)
-			self.rect.y += max(2, travel / 10)
+			target = hand.get_position_from_card(self)
+			if self.mouse_over_me():
+				target -= Vector2(0, 100)
 
-			if abs(travel) < 5:
-				self.hand_locked = False
-				self.rect.bottom = hand.rect.bottom
+			travel = target - self.rect.bottomleft
+			self.rect.bottomleft += travel / 10
 
+			if travel.length() < 1:
+				self.rect.bottomleft = target
 
 	def update_draw(self):
 		if self.dragged:
@@ -93,17 +88,70 @@ class Card(Sprite):
 		pygame.draw.rect(game.windowsystem.screen, self.colour, self.rect, border_radius=5)
 
 
+class Hand(Sprite):
+	LAYER = "MANAGER"
+
+	CARD_SPACING = 20
+
+	@dataclass
+	class CardRep:
+		card: Card
+		idx: int
+
+	def __init__(self, rect):
+		self.rect = rect
+		self.currently_dragged = None
+		self.card_map = []
+
+	def contains(self, card):
+		"""Check if Card is contained in CardRef list"""
+		return self.get_card_rep(card) is not None
+
+	def update_move(self):
+		cglobal = game.sprites.get("CARD")
+		for card in cglobal:
+			if self.get_card_rep(card) is None:
+				self.card_map.append(Hand.CardRep(card, len(self.card_map) - 1))
+
+		game.debug.output(len(self.card_map))
+
+	def get_position_from_card(self, card) -> Vector2:
+		total_width = len(self.card_map) * Hand.CARD_SPACING
+		total_width += sum(ref.card.rect.width for ref in self.card_map)
+
+		start_pos = self.rect.midbottom - Vector2(total_width/2, 0)
+		for ref in sorted(self.card_map, key=lambda ref: ref.idx):
+			if card is ref.card:
+				return start_pos
+			start_pos += Vector2(card.rect.width + Hand.CARD_SPACING, 0)
+
+		return None
+
+	def get_position_from_ref(self, ref) -> Vector2:
+		self.get_position_from_card(ref.card)
+
+
+	def get_card_rep(self, card):
+		"""Get CardRep from Card"""
+		for rep in self.card_map:
+			if rep.card is card:
+				return rep
+		return None
+
+
 def mainmenu():
 	pass
 
 
 def spritefollow():
-	# game.sprites.new(FollowParticle(pos=game.windowsystem.dimensions / 2))
-	game.sprites.new(Card((200, 100), (150, 220), Color("#000000")))
-	game.sprites.new(Card((400, 100), (150, 220), Color("#000000")))
-	game.sprites.new(Card((600, 100), (150, 220), Color("#000000")))
+	game.sprites.new(FollowParticle(pos=game.windowsystem.dimensions / 2))
+	game.sprites.new(Card((200, 100), (150, 220), Color("#ff00ff")))
+	game.sprites.new(Card((400, 100), (150, 220), Color("#ff0000")))
+	game.sprites.new(Card((600, 100), (150, 220), Color("#0000ff")))
+	game.sprites.new(Card((600, 100), (150, 220), Color("#00ff00")))
+	game.sprites.new(Card((600, 100), (150, 220), Color("#ffaa00")))
 
-	game.sprites.HAND = Hand(FRect(0, 740, 1280, 60))
+	game.sprites.HAND = Hand(FRect(0, 780, 1280, 80))
 	game.sprites.new(game.sprites.HAND)
 
 
